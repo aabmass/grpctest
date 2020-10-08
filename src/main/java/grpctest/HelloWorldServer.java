8d9797a3c123d8d803630674d6c115a68548d41b
Aaron Abbott <aaronabbott@google.com>
2020-10-08 16:44:24 +0000

    publish my own metric to test exemplar

--- src/main/java/grpctest/HelloWorldServer.java
+++ src/main/java/grpctest/HelloWorldServer.java
@@ -1,29 +1,34 @@
 package grpctest;
 
-import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
-
-import grpctest.HelloServiceGrpc.HelloServiceImplBase;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.protobuf.services.ProtoReflectionService;
 import io.grpc.stub.StreamObserver;
 import io.opencensus.common.Duration;
+import io.opencensus.contrib.exemplar.util.ExemplarUtils;
 import io.opencensus.contrib.grpc.metrics.RpcViews;
 import io.opencensus.exporter.stats.stackdriver.StackdriverStatsConfiguration;
 import io.opencensus.exporter.stats.stackdriver.StackdriverStatsExporter;
+import io.opencensus.stats.Aggregation;
+import io.opencensus.stats.BucketBoundaries;
+import io.opencensus.stats.MeasureMap;
 import io.opencensus.stats.Stats;
+import io.opencensus.stats.StatsRecorder;
+import io.opencensus.stats.View;
 import io.opencensus.stats.ViewManager;
+import io.opencensus.stats.Aggregation.Distribution;
+import io.opencensus.stats.Measure.MeasureDouble;
+import io.opencensus.stats.View.Name;
 import io.opencensus.trace.config.TraceConfig;
 import io.opencensus.trace.samplers.Samplers;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import io.opencensus.common.Scope;
 import io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
 import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
-import io.opencensus.trace.AttributeValue;
-import io.opencensus.trace.Span;
-import io.opencensus.trace.Status;
 import io.opencensus.trace.Tracer;
 import io.opencensus.trace.Tracing;
 
@@ -31,23 +36,24 @@ public class HelloWorldServer {
 
   private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
   private static final Tracer tracer = Tracing.getTracer();
+  private static final MeasureDouble M_LATENCY_MS = MeasureDouble.create("latency", "The latency in milliseconds",
+      "ms");
 
+  private static final StatsRecorder STATS_RECORDER = Stats.getStatsRecorder();
 
   private Server server;
 
   private void start() throws IOException {
     /* The port on which the server should run */
     int port = 50051;
-    server = ServerBuilder.forPort(port)
-        .addService(new HelloServiceImpl())
-        .addService(ProtoReflectionService.newInstance())
-        .build()
-        .start();
+    server = ServerBuilder.forPort(port).addService(new HelloServiceImpl())
+        .addService(ProtoReflectionService.newInstance()).build().start();
     logger.info("Server started, listening on " + port);
     Runtime.getRuntime().addShutdownHook(new Thread() {
       @Override
       public void run() {
-        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        // Use stderr here since the logger may have been reset by its JVM shutdown
+        // hook.
         System.err.println("*** shutting down gRPC server since JVM is shutting down");
         try {
           HelloWorldServer.this.stop();
@@ -66,12 +72,29 @@ public class HelloWorldServer {
   }
 
   /**
-   * Await termination on the main thread since the grpc library uses daemon threads.
+   * Await termination on the main thread since the grpc library uses daemon
+   * threads.
    */
   private void blockUntilShutdown() throws InterruptedException {
     if (server != null) {
       server.awaitTermination();
     }
+  }
+
+  private static void enableViews() {
+    Aggregation latencyDistribution = Distribution
+        .create(BucketBoundaries.create(Arrays.asList(0.0, 25.0, 100.0, 200.0, 400.0, 800.0, 10000.0)));
+
+    View view = View.create(Name.create("myapp/latency"), "The distribution of the latencies", M_LATENCY_MS,
+        latencyDistribution, Collections.emptyList());
+
+    // Ensure that they are registered so
+    // that measurements won't be dropped.
+    ViewManager manager = Stats.getViewManager();
+    manager.registerView(view);
+
+    // Enable GRPC views
+    RpcViews.registerAllGrpcViews();
   }
 
   /**
@@ -84,30 +107,21 @@ public class HelloWorldServer {
     // For demo purposes, always sample
     TraceConfig traceConfig = Tracing.getTraceConfig();
     traceConfig.updateActiveTraceParams(
-        traceConfig.getActiveTraceParams()
-            .toBuilder()
-            .setSampler(Samplers.alwaysSample())
-            .build());
-
-    // Create the view manager
-    RpcViews.registerAllGrpcViews();
+        traceConfig.getActiveTraceParams().toBuilder().setSampler(Samplers.alwaysSample()).build());
+    enableViews();
 
     // Enable OpenCensus exporters to export metrics to Stackdriver Monitoring.
     // Exporters use Application Default Credentials to authenticate.
-    // See https://developers.google.com/identity/protocols/application-default-credentials
+    // See
+    // https://developers.google.com/identity/protocols/application-default-credentials
     // for more details.
     // The minimum reporting period for Stackdriver is 1 minute.
     // Create the Stackdriver stats exporter
-    StackdriverStatsExporter.createAndRegister(
-        StackdriverStatsConfiguration.builder()
-            .setProjectId(gcpProjectId)
-            .setExportInterval(Duration.create(5, 0))
-            .build());
+    StackdriverStatsExporter.createAndRegister(StackdriverStatsConfiguration.builder().setProjectId(gcpProjectId)
+        .setExportInterval(Duration.create(5, 0)).build());
 
-    StackdriverTraceExporter.createAndRegister(
-        StackdriverTraceConfiguration.builder()
-            .setProjectId(gcpProjectId)
-            .build());
+    StackdriverTraceExporter
+        .createAndRegister(StackdriverTraceConfiguration.builder().setProjectId(gcpProjectId).build());
     server.start();
     server.blockUntilShutdown();
   }
@@ -116,12 +130,14 @@ public class HelloWorldServer {
 
     @Override
     public void sayHello(HelloRequest req, StreamObserver<HelloResponse> responseObserver) {
-      Scope ss = HelloWorldServer.tracer.spanBuilder("grpctest_span").startScopedSpan();
+      Scope ss = tracer.spanBuilder("grpctest_span").startScopedSpan();
+      MeasureMap mmap = STATS_RECORDER.newMeasureMap();
+      mmap.put(M_LATENCY_MS, Math.random() * 100);
+      ExemplarUtils.putSpanContextAttachments(mmap, tracer.getCurrentSpan().getContext());
+      mmap.record();
 
       try {
-        HelloResponse res = HelloResponse.newBuilder()
-            .setResponse("Hello " + req.getMessage() + ", to you!")
-            .build();
+        HelloResponse res = HelloResponse.newBuilder().setResponse("Hello " + req.getMessage() + ", to you!").build();
         responseObserver.onNext(res);
       } finally {
         ss.close();
@@ -130,7 +146,7 @@ public class HelloWorldServer {
     }
   }
 
-  private static String envOrAlternative(String key, String ...alternatives) {
+  private static String envOrAlternative(String key, String... alternatives) {
     String value = System.getenv().get(key);
     if (value != null && value != "")
       return value;
