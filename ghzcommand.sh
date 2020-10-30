84639443869d17df7f727fc344d6b9e96bfd2302
Aaron Abbott <aabmass@gmail.com>
2020-10-29 23:34:13 -0400

    Update ghzcommand.sh

--- ghzcommand.sh
+++ ghzcommand.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ghz --insecure --call grpctest.HelloService.SayHello -d {message: Hello from GHZ Load Test} localhost:50051 -n 100000 -c 10
+ghz --insecure --call grpctest.HelloService.SayHello -d '{"message": "Hello from GHZ Load Test"}' localhost:50051 -n 100000 -c 10
